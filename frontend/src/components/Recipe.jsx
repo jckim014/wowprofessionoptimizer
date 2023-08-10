bf33f23844c746efd9794cc45b89b09b67fee627
Joshua Kim <jckim014@gmail.com>
2023-08-10 00:33:39 -0700

    Frontend displaying more information such as icons and cost

--- frontend/src/components/Recipe.jsx
+++ frontend/src/components/Recipe.jsx
@@ -1,8 +1,33 @@
 const Recipe = ({ recipe }) => {
+  // Add state to record total cost
+
+  // Not sure where price conversion should ultimately go
+  // Initialize gold/silver/copper coins
+  const unconvertedCost = recipe.craftingCost;
+  let gold = 0;
+  let silver = 0;
+  let copper = 0;
+
+  // Calculate gold
+  gold = Math.floor(unconvertedCost / 100);
+  let remainder = unconvertedCost % 100;
+  // Calculate silver
+  silver = remainder;
+  remainder = Math.floor(remainder / 100);
+  // Calculate copper
+  copper = remainder;
+
+  // Eventually add gold/silver/copper coin icons - maybe this can be a local img resource
   return (
     <tr className="recipe">
       <td>{recipe.itemName}</td>
       <td>{recipe.craftedItemID}</td>
+      <td>
+        <img src={`${recipe.icon}`}></img>
+      </td>
+      <td>
+        This item costs {gold} gold, {silver} silver, and {copper} copper.
+      </td>
     </tr>
   );
 };
