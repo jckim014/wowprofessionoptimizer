9fa5d87a22b5b8d593f2825581b04680c8fcbe25
Joshua Kim <jckim014@gmail.com>
2023-09-01 00:42:41 -0700

    Completed recipe dropdowns

--- frontend/src/components/optimalpath/ExpandedRow.jsx
+++ frontend/src/components/optimalpath/ExpandedRow.jsx
@@ -2,10 +2,39 @@ import { useEffect, useState } from "react";
 
 const ExpandedRow = ({ recipe, individualCost, totalCost, reagentInfo }) => {
   return (
-    <tr className="flex border-l border-r border-b border-color -mt-2 px-2 py-2 mb-4 bg-gray-100">
-      {recipe.reagentList.map((reagent, i) => (
-        <td className="p-2">{reagent[0]}</td>
-      ))}
+    <tr className="flex flex-col border-l border-r border-b border-color -mt-2 px-2 py-2 mb-4 bg-gray-100">
+      <div className="flex font-bold p-2 pl-1">Individual Reagents:</div>
+      <div className="flex flex-row">
+        {recipe.reagentList.map((reagent, i) => (
+          <div className="flex pr-8 pb-2 pl-1 flex-wrap" key={i}>
+            <p className="pr-1">x{reagent[1]}</p>
+            {console.log(recipe)}
+            <img
+              className="object-none pr-1"
+              src={`https://wow.zamimg.com/images/wow/icons/tiny/${
+                reagentInfo[reagent[0]].icon
+              }.gif`}
+            ></img>
+            {reagentInfo[reagent[0]].name_enus}
+          </div>
+        ))}
+      </div>
+      <div className="flex font-bold p-2 pl-1">Total Reagents:</div>
+      <div className="flex flex-row">
+        {recipe.reagentList.map((reagent, i) => (
+          <div className="flex pr-8 pb-2 pl-1 flex-wrap" key={i}>
+            <p className="pr-1">x{reagent[1] * recipe.quantityToCraft}</p>
+            {console.log(recipe)}
+            <img
+              className="object-none pr-1"
+              src={`https://wow.zamimg.com/images/wow/icons/tiny/${
+                reagentInfo[reagent[0]].icon
+              }.gif`}
+            ></img>
+            {reagentInfo[reagent[0]].name_enus}
+          </div>
+        ))}
+      </div>
     </tr>
   );
 };
